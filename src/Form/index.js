d116fe461fe3ea3c87348bfc741259b3e8af129b
Artur Feliks <arturfeliks4@gmail.com>
2023-09-26 17:24:16 +0200

    focus() added by using useRef

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { StyledForm, Field, Button } from "./styled";
 
 const Form = ({ addNewTask }) => {
@@ -16,15 +16,18 @@ const Form = ({ addNewTask }) => {
         setNewTaskContent("");
     };
 
+    const inputRef = useRef(null);
+
     return (
         <StyledForm onSubmit={onFormSubmit}>
-                <Field input
-                    value={newTaskContent}
-                    name="newTask"
-                    placeholder="Co jest do zrobienia?"
-                    onChange={({ target }) => setNewTaskContent(target.value)}
-                />
+            <Field input ref={inputRef}
+                value={newTaskContent}
+                name="newTask"
+                placeholder="Co jest do zrobienia?"
+                onChange={({ target }) => setNewTaskContent(target.value)}
+            />
             <Button
+                onClick={() => inputRef.current.focus()}
             >Dodaj zadanie
             </Button>
         </StyledForm>

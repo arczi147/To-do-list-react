72fa18ad1e6d6e9433c53f957e23ff4c802359c8
Artur Feliks <arturfeliks4@gmail.com>
2023-10-11 01:33:03 +0200

    tasksSlice.js added

--- src/useTasks.js
+++ src/useTasks.js
@@ -2,57 +2,10 @@ import { useState, useEffect } from "react";
 
 const useTasks = () => {
     const [tasks, setTasks] = useState(JSON.parse(localStorage.getItem("tasks")) || []);
-    const [hideDone, setHideDone] = useState(false);
 
     useEffect(() => {
         localStorage.setItem("tasks", JSON.stringify(tasks))
     });
-
-    const toggleHideDone = () => {
-        setHideDone(hideDone => !hideDone);
-    };
-
-    const removeTask = (id) => {
-        setTasks(tasks => tasks.filter(task => task.id !== id));
-    };
-
-    const toggleTaskDone = (id) => {
-        setTasks(tasks => tasks.map(task => {
-            if (task.id === id) {
-                return { ...task, done: !task.done };
-            }
-
-            return task;
-        }));
-    };
-
-    const setAllDone = () => {
-        setTasks(tasks => tasks.map(task => ({
-            ...task,
-            done: true,
-        })));
-    };
-
-    const addNewTask = (newTaskContent) => {
-        setTasks(tasks => [
-            ...tasks,
-            {
-                content: newTaskContent,
-                done: false,
-                id: tasks.length === 0 ? 1 : tasks[tasks.length - 1].id + 1,
-            },
-        ]);
-    };
-
-    return {
-        tasks,
-        hideDone,
-        toggleHideDone,
-        removeTask,
-        toggleTaskDone,
-        setAllDone,
-        addNewTask,
-    };
 };
 
 export { useTasks };
